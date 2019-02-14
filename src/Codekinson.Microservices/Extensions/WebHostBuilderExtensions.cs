2bb9cdb9e3d952ef46b35d3b3ee3eaabb00c0f22
Sam <samrtomkinson@gmail.com>
2019-02-14 22:43:28 +0000

    Made use of web host instead of web host builder so I can access the services provider

--- src/Codekinson.Microservices/Extensions/WebHostBuilderExtensions.cs
+++ src/Codekinson.Microservices/Extensions/WebHostBuilderExtensions.cs
@@ -1,9 +1,9 @@
 using System;
-using System.Diagnostics;
 using System.Threading;
 using MassTransit;
 using MassTransit.RabbitMqTransport;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 using RabbitMQ.Client.Exceptions;
 using Serilog;
 
@@ -11,68 +11,48 @@ namespace Codekinson.Microservices.Extensions
 {
     public static class WebHostBuilderExtensions
     {
-        public static IWebHostBuilder EnsureRabbitMqServerAvailable(this IWebHostBuilder builder, string busHost)
-        {
-            return EnsureRabbitMqServerAvailable(builder, busHost, "/");
-        }
-        
-        public static IWebHostBuilder EnsureRabbitMqServerAvailable(this IWebHostBuilder builder, string busHost, string virtualHost)
-        {
-            if (string.IsNullOrWhiteSpace(busHost))
-            {
-                throw new ArgumentNullException(nameof(busHost));
-            }
-
-            if (string.IsNullOrWhiteSpace(virtualHost))
-            {
-                throw new ArgumentNullException(nameof(virtualHost));
-            }
-            
-            return EnsureRabbitMqServerAvailable(
-                builder, 
-                factory => factory.CreateUsingRabbitMq(cfg => { cfg.Host(busHost, virtualHost, h => { }); }),
-                3);
-        }
-        
-        public static IWebHostBuilder EnsureRabbitMqServerAvailable(this IWebHostBuilder builder, Func<IBusFactorySelector, IBusControl> createBusControl, int numberOfRetries)
+        public static IWebHost EnsureRabbitMqServerAvailable(this IWebHost webHost, Func<IServiceProvider, IBusFactorySelector, IBusControl> createBusControl, int numberOfRetries = 3)
         {
             if (createBusControl == null)
             {
                 throw new ArgumentNullException(nameof(createBusControl));
             }
-            
-            var attempts = 0;
-            var available = false;
-            while (!available)
-            {
-                if (attempts > numberOfRetries)
-                {
-                    throw new InvalidOperationException();
-                }
 
-                try
+            using (var scope = webHost.Services.CreateScope())
+            {
+                var attempts = 0;
+                var available = false;
+                while (!available)
                 {
-                    var factory = Bus.Factory;
-                    var control = createBusControl(factory);
-                    control.Start();
-                    
-                    available = true;
-                    Log.Information("Event bus available, continuing");
-                }
-                catch (RabbitMqConnectionException exception)
-                    when (exception.InnerException is BrokerUnreachableException)
-                {
-                    available = false;
-                    Log.Information("Unable to connect to the rabbit queue, retrying in 5 seconds");
-                    Thread.Sleep(5000);
-                }
-                finally
-                {
-                    attempts += 1;
+                    if (attempts > numberOfRetries)
+                    {
+                        throw new InvalidOperationException();
+                    }
+
+                    try
+                    {
+                        var factory = Bus.Factory;
+                        var control = createBusControl(scope.ServiceProvider, factory);
+                        control.Start();
+
+                        available = true;
+                        Log.Information("Event bus available, continuing");
+                    }
+                    catch (RabbitMqConnectionException exception)
+                        when (exception.InnerException is BrokerUnreachableException)
+                    {
+                        available = false;
+                        Log.Information("Unable to connect to the rabbit queue, retrying in 5 seconds");
+                        Thread.Sleep(5000);
+                    }
+                    finally
+                    {
+                        attempts += 1;
+                    }
                 }
             }
-            
-            return builder;
+
+            return webHost;
         }
     }
 }
